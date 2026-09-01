09c30e84282efd6b3a30570e5a0af9cca654f059
modymarzouk00-debug <modymarzouk00@gmail.com>
2026-09-01 22:08:13 +0300

    Add files via upload

---
+++ main.dart
@@ -0,0 +1,21 @@
+import 'package:flutterflow_ui/flutterflow_ui.dart';
+import 'package:flutterflow_ui/flutterflow_ui.dart';
+import 'package:flutter/material.dart';
+import 'page1.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'My App',
+      debugShowCheckedModeBanner: false,
+      home: const HomePageWidget(),
+    );
+  }
+}
